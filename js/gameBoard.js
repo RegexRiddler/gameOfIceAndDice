e2205470d2c2f1f4b19bb19a88210b04f7d02f5b
Maximilian Dybvik <maximilian.dybvik@gmail.com>
2018-12-11 06:09:37 +0100

    Finalized the game, only missing logo.

--- js/gameBoard.js
+++ js/gameBoard.js
@@ -6,7 +6,7 @@ const chosenCharacter = JSON.parse(retrievedCharacter)
 let dieRoll = null;
 let isPlayerTurn = true;
 let playerPosition = 1;
-let npcPosition = 5;
+let npcPosition = 1;
 let playerPenaltyCounter = null;
 let npcPenaltyCounter = null;
 let playerDiePenalty = null;
@@ -24,10 +24,14 @@ rollButton.addEventListener("click", function() {
   }
 });
 
+// Event handler for dismissing trap cards for player traps //
 dismissTrapCard.addEventListener("click", function() {
   document.getElementById("trapCard").style.display = "none";
+  // Only when card is dismissed can npc take its turn //
+  moveNpc();
 });
 
+// Event handler for dismissing trap cards for npc traps //
 dismissNpcTrapCard.addEventListener("click", function() {
   document.getElementById("npcTrapCard").style.display = "none";
 });
@@ -37,14 +41,22 @@ function resetPreviousPosition(npcOrPlayer) {
   switch (npcOrPlayer) {
     case "player":
       let playerCurrentPosition = document.getElementById(`pos${playerPosition}`);
-      playerCurrentPosition.querySelector("img").style.display = "none";
-      playerCurrentPosition.querySelector("span").innerHTML = `${playerPosition}`;
+      if (npcPosition === playerPosition) {
+        playerCurrentPosition.querySelector("img").style.display = "block";
+        playerCurrentPosition.querySelector("img").src = "images/avatars/white-walker.png"; }
+      else {
+        playerCurrentPosition.querySelector("img").style.display = "none";
+        playerCurrentPosition.querySelector("span").innerHTML = `${playerPosition}`; }
       break;
 
     case "npc":
       let npcCurrentPosition = document.getElementById(`pos${npcPosition}`);
-      npcCurrentPosition.querySelector("img").style.display = "none";
-      npcCurrentPosition.querySelector("span").innerHTML = `${npcPosition}`;
+      if (playerPosition === npcPosition) {
+        npcCurrentPosition.querySelector("img").style.display = "block";
+        npcCurrentPosition.querySelector("img").src = `${chosenCharacter.avatar}`; }
+      else {
+        npcCurrentPosition.querySelector("img").style.display = "none";
+        npcCurrentPosition.querySelector("span").innerHTML = `${npcPosition}`; }
       break;
   }  
 }
@@ -90,103 +102,85 @@ function setNewPosition(npcOrPlayer, dieRoll) {
 
 // The function that handle the players turn from start to finish //
 function movePlayer() {
+  dieRoll = Math.floor((Math.random() * 6) + 1);
+  document.getElementById("die").innerHTML = dieRoll;
+
   if (playerPenaltyCounter > 0) {
     playerPenaltyCounter--
-    moveNpc(); }
+    moveNpc(); }  
   else if (playerDiePenalty > 0) {
-    dieRoll = Math.floor((Math.random() * 6) + 1) - playerDiePenalty;
-    document.getElementById("die").innerHTML = dieRoll + playerDiePenalty;
+    dieRoll -= playerDiePenalty;
     playerDiePenalty = 0;
-
+    isPlayerTurn = false;
+    // document.getElementById("rollButton").innerHTML = "roll die";
     resetPreviousPosition("player");
     setNewPosition("player", dieRoll);
-    
-    isPlayerTurn = false;
-
-    document.getElementById("rollButton").innerHTML = "wait turn";
-
     moveNpc(); }
+  else if (playerPosition + dieRoll === 7 || playerPosition + dieRoll === 10 || playerPosition + dieRoll === 13 || playerPosition + dieRoll === 21 || playerPosition + dieRoll === 24 || playerPosition + dieRoll === 28) {
+    document.getElementById("rollButton").innerHTML = "wait turn";
+    isPlayerTurn = false;
+    resetPreviousPosition("player");
+    setNewPosition("player", dieRoll);
+    playerTraps(playerPosition); }
+  else if (dieRoll === 6) {
+    document.getElementById("rollButton").innerHTML = "roll again";
+    resetPreviousPosition("player");
+    setNewPosition("player", dieRoll); } 
   else {
-    dieRoll = Math.floor((Math.random() * 6) + 1);
-    document.getElementById("die").innerHTML = dieRoll;
-    
-    // If player rolls a 6, take another turn//
-    if (dieRoll === 6) {
-      resetPreviousPosition("player");
-      setNewPosition("player", dieRoll);
-        
-      if (playerPosition === 7 || playerPosition === 10 || playerPosition === 13 || playerPosition === 21 || playerPosition === 24 || playerPosition === 28) {
-        playerTraps(playerPosition);
-      }
-      document.getElementById("rollButton").innerHTML = "roll again"; } 
-    // Otherwise play as normal //
-    else {
-      resetPreviousPosition("player");
-      setNewPosition("player", dieRoll);
-        
-      if (playerPosition === 7 || playerPosition === 10 || playerPosition === 13 || playerPosition === 21 || playerPosition === 24 || playerPosition === 28) {
-        playerTraps(playerPosition);
-      }
-      
-      isPlayerTurn = false;
-
-      document.getElementById("rollButton").innerHTML = "wait turn";
-      moveNpc();
-    }
+    document.getElementById("rollButton").innerHTML = "wait turn";
+    isPlayerTurn = false;
+    resetPreviousPosition("player");
+    setNewPosition("player", dieRoll);
+    moveNpc();
   }
 }
 
 // The function that handles the NPC turn from start to finish //
 function moveNpc() {
-  if (npcPenaltyCounter > 0) {
-    npcPenaltyCounter--
-    document.getElementById("rollButton").innerHTML = "take another turn"; } 
-  else if (npcDiePenalty > 0) {
-    dieRoll = Math.floor((Math.random() * 6) + 1) - npcDiePenalty;
-    npcDiePenalty = 0;
+  dieRoll = Math.floor((Math.random() * 6) + 1);
 
-    setTimeout(() => {
+  setTimeout(() => {
+    if (npcPenaltyCounter > 0) {
+      npcPenaltyCounter--
+      isPlayerTurn = true;
+      if (playerPenaltyCounter > 0) {
+        document.getElementById("rollButton").innerHTML = "skip turn"; }
+      else {
+        document.getElementById("rollButton").innerHTML = "roll again"; }
+    } 
+    else if (npcDiePenalty > 0) {
+      dieRoll -= npcDiePenalty;
+      npcDiePenalty = 0;
+      resetPreviousPosition("npc");
+      setNewPosition("npc", dieRoll); 
+      if (playerPenaltyCounter > 0) {
+        document.getElementById("rollButton").innerHTML = "skip turn"; } 
+      else {
+        document.getElementById("rollButton").innerHTML = "roll die"; }
+      isPlayerTurn = true; }
+    else if (npcPosition + dieRoll === 7 || npcPosition + dieRoll === 10 || npcPosition + dieRoll === 13 || npcPosition + dieRoll === 21 || npcPosition + dieRoll === 24 || npcPosition + dieRoll === 28) {
+      resetPreviousPosition("npc");
+      setNewPosition("npc", dieRoll); 
+      npcTraps(npcPosition);
+      if (playerPenaltyCounter > 0) {
+        document.getElementById("rollButton").innerHTML = "skip turn"; } 
+      else {
+        document.getElementById("rollButton").innerHTML = "roll die"; }
+      isPlayerTurn = true; }
+    else {
       resetPreviousPosition("npc");
       setNewPosition("npc", dieRoll);
-
-      if (npcPosition === 7 || npcPosition === 10 || npcPosition === 13 || npcPosition === 21 || npcPosition === 24 || npcPosition === 28) {
-        npcTraps(npcPosition);
-      }
-  
       if (playerPenaltyCounter > 0) {
-        document.getElementById("rollButton").innerHTML = "skip turn";
-      } else {
-        document.getElementById("rollButton").innerHTML = "roll die";
-      }
-
-      isPlayerTurn = true;
-  
-    }, 500); }
-  else {
-    setTimeout(() => {
-      dieRoll = Math.floor((Math.random() * 0) + 1);
-      
-      resetPreviousPosition("npc");
-      setNewPosition("npc", dieRoll);
-
-      if (npcPosition === 7 || npcPosition === 10 || npcPosition === 13 || npcPosition === 21 || npcPosition === 24 || npcPosition === 28) {
-        npcTraps(npcPosition);
-      }
-  
-      if (playerPenaltyCounter > 0) {
-        document.getElementById("rollButton").innerHTML = "skip turn";
-      } else {
-        document.getElementById("rollButton").innerHTML = "roll die";
-      }
-
-      isPlayerTurn = true;
-  
-    }, 500);
-  }
+        document.getElementById("rollButton").innerHTML = "skip turn"; } 
+      else {
+        document.getElementById("rollButton").innerHTML = "roll die"; }
+      isPlayerTurn = true;    
+    }
+  }, 600);
 }
 
+// Function for handling player traps //
 function playerTraps(trapTile) {
-  pause = true;
   setTimeout(() => {
     const trapCard = document.getElementById("trapCard");
     trapCard.style.display = "block";
@@ -194,7 +188,7 @@ function playerTraps(trapTile) {
 
   switch (trapTile) {
     case 7:
-        trapCard.querySelector("span").innerHTML = "you try to swim but your gear is too heavy, so you swim back to build a raft instead";
+        trapCard.querySelector("span").innerHTML = "you try to swim but your gear is too heavy. you swim back to build a raft instead";
 
       setTimeout(() => {
         resetPreviousPosition("player");
@@ -203,7 +197,7 @@ function playerTraps(trapTile) {
       break;
       
     case 10:
-        trapCard.querySelector("span").innerHTML = "With an improvised raft you have made it almost to the shore of your destination, but you are met with strong currents and are dragged back to sea";
+        trapCard.querySelector("span").innerHTML = "with an improvised raft you have made it almost to the shore of your destination, but you are met with strong currents and are dragged back to sea";
 
       setTimeout(() => {
         resetPreviousPosition("player");
@@ -212,17 +206,17 @@ function playerTraps(trapTile) {
       break;
       
     case 13:
-        trapCard.querySelector("span").innerHTML = "Your journey has taken you to the far east of westeros, you are tired and hungry and decide to spend the night at a quiet inn. you wake up noticing your valuables are gone! You spend the next 2 turns begging";
+        trapCard.querySelector("span").innerHTML = "your journey has taken you to the far east of westeros, you are tired and hungry, and decide to spend the night at a quiet inn. you wake up noticing your valuables are gone! you spend the next 2 turns tracking the thieves";
         playerPenaltyCounter = 2;
       break;
       
     case 21:
-      trapCard.querySelector("span").innerHTML = "You are traveling high in the mountains and need to acclimate to the increasing altitudes. Rest for 1 turn";
+      trapCard.querySelector("span").innerHTML = "you are traveling high in the mountains and need to acclimate to the increasing altitudes. rest for 1 turn";
       playerPenaltyCounter = 1;
     break;
 
     case 24:
-        trapCard.querySelector("span").innerHTML = "You find yourself in a tavern brawl, outnumbered you are knocked unconsious. the next day you wake up in a haze of confusion and travel in the wrong direction 2 paces";
+        trapCard.querySelector("span").innerHTML = "you find yourself in a tavern brawl, outnumbered you are knocked unconsious. the next day you wake up in a haze of confusion and travel 2 paces in the wrong direction";
 
       setTimeout(() => {
         resetPreviousPosition("player");
@@ -231,61 +225,63 @@ function playerTraps(trapTile) {
       break;
 
     case 28:
-        trapCard.querySelector("span").innerHTML = "You have made it to the north, cold and slowed by the snow. Next turn subtract 3 from your die roll";
+        trapCard.querySelector("span").innerHTML = "you have made it to the north, cold and slowed by the snow. next turn subtract 3 from your die roll";
         playerDiePenalty = 3;
       break;
   }
 }
 
+// Function for handling NPC traps //
 function npcTraps(trapTile) {
     const npcTrapCard = document.getElementById("npcTrapCard");
     npcTrapCard.style.display = "block";
 
   switch (trapTile) {
     case 7:
-        npcTrapCard.querySelector("span").innerHTML = "the white walkers boats need repair so they head back to mainland";
+      npcTrapCard.querySelector("span").innerHTML = "the white walkers boats need repair, so they head back to mainland";
 
       setTimeout(() => {
         resetPreviousPosition("npc");
         setNewPosition("npc", -1);
-      }, 500);
+      }, 600);
       break;
       
     case 10:
-        npcTrapCard.querySelector("span").innerHTML = "a massive storm is brewing, the white walkers are forced to wait it out in the eye of the storm";
+      npcTrapCard.querySelector("span").innerHTML = "a massive storm is brewing, the white walkers are forced to wait it out in the eye of the storm";
 
       setTimeout(() => {
         resetPreviousPosition("npc");
         setNewPosition("npc", -1);
-      }, 500);
+      }, 600);
       break;
       
     case 13:
-        npcTrapCard.querySelector("span").innerHTML = "westeros east coast is no place for a white walker, they meet a sandstorm and are burried alive. the spend 2 turns gathering their forces again";
-        npcPenaltyCounter = 2;
+      npcTrapCard.querySelector("span").innerHTML = "westeros east coast is no place for a white walker, they meet a sandstorm and are burried. they spend the next 2 turns gathering their forces";
+      npcPenaltyCounter = 2;
       break;
       
     case 21:
-      npcTrapCard.querySelector("span").innerHTML = "The mountain path is closed, the white walkers spend the next turn finding another way";
+      npcTrapCard.querySelector("span").innerHTML = "the mountain path has colappsed, and the white walkers spend the next turn finding another way";
       npcPenaltyCounter = 1;
-    break;
+      break;
 
     case 24:
-        npcTrapCard.querySelector("span").innerHTML = "The white walkers meet one of khaleesis rogue dragons and must retreat 2 paces";
+      npcTrapCard.querySelector("span").innerHTML = "the white walkers meet one of khaleesis rogue dragons and must retreat 2 paces";
 
       setTimeout(() => {
         resetPreviousPosition("npc");
         setNewPosition("npc", -2);
-      }, 500);
+      }, 600);
       break;
 
     case 28:
-        npcTrapCard.querySelector("span").innerHTML = "an army of wildlings ambushed the white walkers, their way forth is slowed down. The white walkers ned die roll gets 3 subtracted";
-        npcDiePenalty = 3;
+      npcTrapCard.querySelector("span").innerHTML = "an army of wildlings are ambushing the white walkers, their way forth is slowed down. The white walkers next die roll gets 3 subtracted";
+      npcDiePenalty = 3;
       break;
   }
 }
 
+// Function for displaying victory/defeat message //
 function displayEndGameMessage(npcOrPlayer) {
   switch (npcOrPlayer) {
     case "player":
